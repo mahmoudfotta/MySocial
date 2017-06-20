8380474704c4041923a427c89226535a6e380f28
mahmoud gamal <mahmoudgamal@192.168.85.128>
2017-06-20 14:15:53 +0200

    upload images from firebase

--- MySocial/FeedVC.swift
+++ MySocial/FeedVC.swift
@@ -12,11 +12,13 @@ import Firebase
 
 class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
+    @IBOutlet weak var captionField: CustomTextField!
     @IBOutlet weak var imageAdd: CircleImageView!
     var imagePicker: UIImagePickerController!
     @IBOutlet weak var feedTable: UITableView!
     var posts = [Post]()
     static var imageCache: NSCache<NSString, UIImage> = NSCache()
+    var imageSelected = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -57,9 +59,36 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UIIm
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         if let image = info[UIImagePickerControllerEditedImage] as? UIImage {
             imageAdd.image = image
+            imageSelected = true
         }
         imagePicker.dismiss(animated: true, completion: nil)
     }
+    
+    @IBAction func postBtnTapped(_ sender: AnyObject) {
+        guard let caption = captionField.text, caption != "" else {
+            print("Caption must be entered")
+            return
+        }
+        guard let img = imageAdd.image, imageSelected == true else {
+            print("Image must be selected")
+            return
+        }
+        
+        if let imageData = UIImageJPEGRepresentation(img, 0.2) {
+            let imageUid = UUID().uuidString
+            let metaData = FIRStorageMetadata()
+            metaData.contentType = "image/jpeg"
+            DataService.ds.REF_POST_IMAGES.child(imageUid).put(imageData, metadata: metaData, completion: { (metaData, error) in
+                if error == nil {
+                    let dawnloadUrl = metaData?.downloadURL()?.absoluteString
+                    print("SUCCESS \(dawnloadUrl)")
+                } else {
+                    print("ERROR \(error?.localizedDescription)")
+                }
+            })
+        }
+    }
+    
     // MARK: tableView
     
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -72,6 +101,7 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UIIm
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if let cell = feedTable.dequeueReusableCell(withIdentifier: "PostCell", for: indexPath) as? PostCell {
+            print(FeedVC.imageCache.countLimit)
             if let img = FeedVC.imageCache.object(forKey: posts[indexPath.row].imageUrl as NSString) {
                 cell.configureCell(post: posts[indexPath.row], img: img)
                 return cell
